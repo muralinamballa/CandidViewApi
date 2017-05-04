8e23a0c944bd20f9d7767ba476c029ccc0f88d9f
Murali.Namballa <Murali.Namballa@LTPUSSJO2143602>
2017-05-04 13:43:08 -0500

    Initial Commit with sample api

---
+++ WebApplication1/WebApplication1/Controllers/ProjectInfoController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [EnableCors(origins: "http://localhost:3000", headers: "*", methods: "*")]
+    public class ProjectInfoController : ApiController
+    {
+        // GET api/projectinfo
+        public Info[] Get()
+        {
+            var dic = new Dictionary<long, Info>();
+            dic.Add(1, new Info { id = 1, name = "CARE - Number of defects 10" });
+            dic.Add(2, new Info { id = 2, name = "Budget Variance - 17%" });
+            return dic.Values.ToArray();
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/Models/project.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApplication1.Controllers
+{
+    public class Info
+    {
+        public long id { get; set; }
+        public string name { get; set; }
+    }
+}
