0a6f1cbae9e98f24d854b482c6e40bca47af4f6b
MaheshBabu25 <Mahesh.Babu@mphasis.com>
2017-05-24 16:24:51 +0530

    Added RiskStatus Features

---
+++ WebApplication1/WebApplication1/Controllers/StatusController.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Http;
+using System.Web.Http.Cors;
+using Newtonsoft.Json;
+using CandidView.Models.RiskStatus;
+using System.IO;
+using System.Web;
+using System.Collections.Generic;
+
+namespace CandidView.Controllers
+{
+    [EnableCors(origins: "http://localhost:3000", headers: "*", methods: "*")]
+    public class StatusController : ApiController
+    {
+        [HttpGet]
+        public List<RiskStatus> GetRiskStatus()
+        {
+            List<RiskStatus> data = JsonConvert.DeserializeObject<List<RiskStatus>>(File.ReadAllText(HttpContext.Current.Server.MapPath("/data/riskstatus.json")));
+            return data;
+        }
+       
+    }
+}
---
+++ WebApplication1/WebApplication1/Models/RiskStatus/RiskStatus.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+
+namespace CandidView.Models.RiskStatus
+{
+    public class RiskStatus
+    {
+        [JsonProperty(PropertyName = "slno")]
+        int Slno { get; set; }
+        [JsonProperty(PropertyName = "type")]
+        string Type { get; set; }
+        [JsonProperty(PropertyName = "description")]
+        string Description { get; set; }
+        [JsonProperty(PropertyName = "owner")]
+        string Owner { get; set; }
+        [JsonProperty(PropertyName = "exposure")]
+        string Exposure { get; set; }
+        [JsonProperty(PropertyName = "status")]
+        string Status { get; set; }
+    }
+}
